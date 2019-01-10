aa61d31333cf81108649512283f0f73a65376000
Stasqq <stachocz@gmail.com>
2019-01-10 12:33:50 +0100

    Scheme class

---
+++ Scheme.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Stasiek on 2019-01-10.
+//
+
+#include "Scheme.h"
+
+
+Scheme::Scheme() {
+    firstNode=nullptr;
+    lastNode= nullptr;
+}
---
+++ Scheme.h
@@ -0,0 +1,19 @@
+//
+// Created by Stasiek on 2019-01-10.
+//
+
+#ifndef PROI_3RD_SCHEME_H
+#define PROI_3RD_SCHEME_H
+
+#include "Element.h"
+
+class Scheme {
+private:
+    Node* firstNode;
+    Node* lastNode;
+public:
+    Scheme();
+};
+
+
+#endif //PROI_3RD_SCHEME_H
